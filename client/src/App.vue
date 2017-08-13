c3bf761d00a02f79767f98beeb97cfc4b36a0a9a
ThePizar <joshi.michel@gmail.com>
2017-08-12 23:22:30 -0400

    Vure routing

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <nav class="navbar">
       <div class="container nav-background">
+        <router-link to="/" class="navbar-item">Home</router-link>
         <a class="navbar-item" href="https://github.com/ThePizar/pizar-site/">Github</a>
+        <router-link to="/poems" class="navbar-item">Poetry</router-link>
       </div>
     </nav>
     <h1 class="large has-text-centered" style="font-size: 48px">{{ msg }}</h1>
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,20 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App.vue';
 
+const routes = [
+  {path: '/'},
+  {path: '/poems'},
+];
+
+const router = new VueRouter({
+  routes
+});
+
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
 });
