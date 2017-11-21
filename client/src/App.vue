aae7fe0fc87a564c896c2c3459a33ee11ca0a5a4
ThePizar <joshi.michel@gmail.com>
2017-11-21 00:51:09 -0500

    Start layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,5 @@
 <template>
-  <div id="app">
-    <nav class="navbar">
-      <div class="container nav-background">
-        <router-link to="/" class="navbar-item">Home</router-link>
-        <a class="navbar-item" href="https://github.com/ThePizar/pizar-site/">Github</a>
-        <router-link to="/poems" class="navbar-item">Poetry</router-link>
-      </div>
-    </nav>
+  <div id="app" class="is-fullheight">
     <router-view></router-view>
   </div>
 </template>
@@ -16,14 +9,8 @@
     name: 'app',
     data () {
       return {
-        msg: 'Welcome to Pizar\'s Site'
+        msg: 'Campfir'
       }
     }
   }
 </script>
-
-<style scoped>
-  .nav-background {
-    background: #47d1b1
-  }
-</style>
--- client/src/components/main.vue
+++ client/src/components/main.vue
@@ -1,5 +1,25 @@
 <template>
-  <h1 class="large has-text-centered title is-1">{{ title }}</h1>
+  <div class="columns is-fullheight">
+    <div class="column is-2 nav-background">
+      <h1 class="large has-text-centered title">
+        {{title}}
+      </h1>
+      <div class="is-sidebar-menu">
+        <aside class="menu">
+        </aside>
+      </div>
+    </div>
+    <div class="column">
+      <h1 class="large has-text-centered title is-1">Music feed</h1>
+      <div class="tile is-ancestor">
+        <div class="is-parent">
+          <div class="tile is-child box" v-for="entry in entries">
+            {{entry.name}}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +27,25 @@
     name: 'main',
     data () {
       return {
-        title: 'Welcome to Pizar\'s Site'
+        title: 'Campfir',
+        entries: [
+          {
+            name: 'Cat'
+          },
+          {
+            name: 'Dog'
+          },
+          {
+            name: 'Cow'
+          }
+        ]
       }
     }
   }
 </script>
+
+<style scoped>
+  .nav-background {
+    background: #d17231
+  }
+</style>
